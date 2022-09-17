b64a71e5b36402800f30d54b1be8742a27c8501e
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2022-09-17 18:43:24 -0500

    login done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'login-concept';
+
+  public email: string = '';
+  public password: string = '';
+
+  public login() {
+    console.log(this.email, this.password);
+  }
 }
